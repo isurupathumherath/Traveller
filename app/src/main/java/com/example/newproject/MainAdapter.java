ee9a4eb5641098e4de41b0d86c8035907ba64fc8
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-09-26 12:45:28 +0530

    new update

--- app/src/main/java/com/example/newproject/MainAdapter.java
+++ app/src/main/java/com/example/newproject/MainAdapter.java
@@ -1,10 +1,16 @@
 package com.example.newproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,8 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
+
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,6 +34,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<Guide, MainAdapter.myVi
      *
      * @param options
      */
+
     public MainAdapter(@NonNull FirebaseRecyclerOptions<Guide> options) {
         super(options);
     }
@@ -40,7 +50,25 @@ public class MainAdapter extends FirebaseRecyclerAdapter<Guide, MainAdapter.myVi
                 load(model.getImage()).
                 placeholder(R.drawable.common_google_signin_btn_icon_dark).circleCrop()
                 .error(R.drawable.common_google_signin_btn_icon_dark_normal).into(holder.img);
+
+
+        holder.btnaddcommit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(v.getContext(), activity_guide_review.class);
+                i.putExtra("name", model.getName());
+                i.putExtra("image", model.getImage());
+                v.getContext().startActivity(i);
+
+
+            }
+        });
     }
+
+
+
+
 
     @NonNull
     @Override
@@ -53,16 +81,25 @@ public class MainAdapter extends FirebaseRecyclerAdapter<Guide, MainAdapter.myVi
 
         CircleImageView img;
         TextView name, contact, email, type;
-
+        Button btnaddcommit;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
+
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent i = new Intent(v.getContext(), activity_guide_review.class);
+//                    i.putExtra("title", data.get)
+//                }
+//            });
 
             img = (CircleImageView)itemView.findViewById(R.id.img1);
             name = (TextView)itemView.findViewById(R.id.nametext);
             contact = (TextView)itemView.findViewById(R.id.contacttext);
             email = (TextView)itemView.findViewById(R.id.emailtext);
             type = (TextView)itemView.findViewById(R.id.typetext);
+            btnaddcommit = (Button)itemView.findViewById(R.id.btnaddcomment);
 
 
         }
