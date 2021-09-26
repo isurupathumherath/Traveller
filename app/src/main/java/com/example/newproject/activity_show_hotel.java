2e68e71f2039c8e1c07779c3d692d1b825318d87
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-09-26 14:58:26 +0530

    Hotel All View Added

--- app/src/main/java/com/example/newproject/activity_show_hotel.java
+++ app/src/main/java/com/example/newproject/activity_show_hotel.java
@@ -18,8 +18,6 @@ public class activity_show_hotel extends AppCompatActivity {
 
     RecyclerView recyclerView;
     MainAdapterHotel mainAdapterHotel;
-    Button addComment, viewComments;
-    TextView tv ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +25,6 @@ public class activity_show_hotel extends AppCompatActivity {
         setContentView(R.layout.activity_show_hotel);
 
         recyclerView = (RecyclerView)findViewById(R.id.rvAllHotel);
-        addComment = (Button)findViewById(R.id.btnaddcomment);
-        viewComments = (Button)findViewById(R.id.btnseecomment);
-        tv = (TextView)findViewById(R.id.nametext);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -55,38 +50,5 @@ public class activity_show_hotel extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mainAdapterHotel.stopListening();
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.search, menu);
-        MenuItem item = menu.findItem(R.id.search22);
-        SearchView searchView = (SearchView) item.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                txtSearch(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String query) {
-                txtSearch(query );
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    private void txtSearch(String str){
-        FirebaseRecyclerOptions<Hotel> options =
-                new FirebaseRecyclerOptions.Builder<Hotel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Hotel").orderByChild("name").startAt(str).endAt(str+"~"), Hotel.class)
-                        .build();
-
-        mainAdapterHotel = new MainAdapterHotel(options);
-        mainAdapterHotel.startListening();
-        recyclerView.setAdapter(mainAdapterHotel);
     }
 }
