9f53fefb328750749cf60e0ce86808965106397f
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-09-26 13:07:18 +0530

    Show Hotel Activity Created

---
+++ app/src/main/java/com/example/newproject/activity_show_hotel.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_show_hotel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_hotel);
+    }
+}
